922a4d8cff9f5b5daf4da7a4a902e7af3d5c696a
Jim Barrett <barrettj1971@gmail.com>
2022-04-25 11:56:57 -0400

    fix install failures

--- lamp-install.sh
+++ lamp-install.sh
@@ -1,31 +1,34 @@
 #!/bin/sh
 
 echo "installing packages..."
-pacman --no-confirm --needed -S apache php php-apache mariadb composer >/dev/null 2>&1
+yes | pacman -S apache php php-apache mariadb composer &&
 
-echo "setting up db..."
-mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+echo "setting up db..." &&
+mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql &&
 
-echo "updating .conf..."
+echo "updating .conf..." &&
 awk '{
 	if ($NF ~ /mod_unique_id/)
 		$0="#"$0
 	if ($NF ~ /mod_mpm_event/)
 		$0="#"$0
 	print
-}' /etc/httpd/conf/httpd.conf > newhttpd
+}' /etc/httpd/conf/httpd.conf > newhttpd &&
 
-echo "LoadModule mpm_prefork_module modules/mod_mpm_prefork.so" >> newhttpd
-echo "LoadModule php_module modules/libphp.so" >> newhttpd
-echo "AddHandler php-script .php" >> newhttpd
-echo "Include conf/extra/php_module.conf" >> newhttpd
+echo "LoadModule mpm_prefork_module modules/mod_mpm_prefork.so" >> newhttpd &&
+echo "LoadModule php_module modules/libphp.so" >> newhttpd &&
+echo "AddHandler php-script .php" >> newhttpd &&
+echo "Include conf/extra/php_module.conf" >> newhttpd &&
 
-chgrp -R http /srv/http
-chown -R g+w /srv/http
+chgrp -R http /srv/http &&
+chmod -R g+w /srv/http&&
 
-systemctl enable httpd
-systemctl enable mariadb
+systemctl start httpd &&
+systemctl enable httpd &&
+systemctl start mariadb &&
+systemctl enable mariadb &&
 
 clear
-echo "Installation complete. Don't forget to run mysql_secure_installation."
+echo "Installation complete. Don't forget to run mysql_secure_installation and add your user to the http group "
+echo "usermod -a -G http <your-user>"
 
